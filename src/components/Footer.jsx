21d0fe4c7c59e211c5e4d90e24c112693f188a9e
HAndGDigital <133459377+HAndGDigital@users.noreply.github.com>
2023-10-11 21:44:33 -0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logoMain from "../assets/images/logo.png";
+import logoMain from "../assets/images/Logo.png";
 function Footer() {
   return (
     <>
